ce05b931f2b30949efb60f899d4ff02c5a502374
Darren Gardner <jamerican10@hotmail.comgit config --global user.name DGWonKanobi>
2024-02-21 23:35:03 -0500

    feat added : created logic in chatbot view to save both message and response history of the user - succesfully viewing admin chat and response history on admin page

--- chatbot/views.py
+++ chatbot/views.py
@@ -4,8 +4,10 @@ from openai import OpenAI
 import openai
 from django.contrib import auth
 from django.contrib.auth.models import User
+from .models import Chat
+from django.utils import timezone
 
-openai_api_key = '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'
+openai_api_key = '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'
 client = OpenAI(api_key=openai_api_key)
 
 
@@ -23,11 +25,16 @@ def ask_openai(message):
 
 # Create your views here.
 def chatbot(request):
+    chats = Chat.objects.filter(user=request.user)
+
     if request.method == 'POST':
       message = request.POST.get('message')
       response = ask_openai(message)
+
+      chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+      chat.save()
       return JsonResponse({'message': message, 'response': response })
-    return render(request, 'chatbot.html')
+    return render(request, 'chatbot.html', {'chats': chats})
 
 def login(request):
    if request.method == 'POST':
