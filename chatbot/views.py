bee6f20b0f9ec60b871785adddd3a172b3c0a956
Darren Gardner <jamerican10@hotmail.comgit config --global user.name DGWonKanobi>
2024-02-21 22:06:46 -0500

    feat: added login view logic. successfully logged user in from login page added logout redirect to logout view

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,7 +5,7 @@ import openai
 from django.contrib import auth
 from django.contrib.auth.models import User
 
-openai_api_key = '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'
+openai_api_key = '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'
 client = OpenAI(api_key=openai_api_key)
 
 
@@ -30,48 +30,61 @@ def chatbot(request):
     return render(request, 'chatbot.html')
 
 def login(request):
-   return render(request, 'login.html')
-
-# def register(request):
-#    if request.method == 'POST':
-#       username = request.POST['username']
-#       email = request.POST['email']
-#       password1 = request.POST['password1']
-#       password2 = request.POST['password2']
-
-#       if password1 == password2:
-#         try:
-#              user = User.objects.create_user(username, email, password1)
-#              user.save()
-#              auth.login(request, user)
-#              return redirect('chatbot')
-#         except:
-#            error_message = 'Error Creating Your Account'
-#            return render(request, 'register.html', {'error_message': error_message})
-#       else:
-#           error_message = 'Passwords Do Not Match'
-#           return render(request, 'register.html', {'error_message': error_message})
-#    return render(request, 'register.html')
+   if request.method == 'POST':
+      username = request.POST['username']
+      password = request.POST['password']
+      user = auth.authenticate(request, username=username, password=password)
+      if user is not None:
+         auth.login(request, user)
+         return redirect('chatbot')
+      else:
+         error_message = 'Invalid username or password'
+         return render(request, 'login.html', {'error_message': error_message})
+   else:
+        return render(request, 'login.html')
 
 def register(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
+   if request.method == 'POST':
+      username = request.POST['username']
+      email = request.POST['email']
+      password1 = request.POST['password1']
+      password2 = request.POST['password2']
 
-        if password1 == password2:
-            try:
-                user = User.objects.create_user(username, email, password1)
-                user.save()
-                auth.login(request, user)
-                return redirect('chatbot')
-            except:
-                error_message = 'Error creating account'
-                return render(request, 'register.html', {'error_message': error_message})
-        else:
-            error_message = 'Password dont match'
-            return render(request, 'register.html', {'error_message': error_message})
-    return render(request, 'register.html')
+      if password1 == password2:
+        try:
+             user = User.objects.create_user(username, email, password1)
+             user.save()
+             auth.login(request, user)
+             return redirect('chatbot')
+        except:
+           error_message = 'Error Creating Your Account'
+           return render(request, 'register.html', {'error_message': error_message})
+      else:
+          error_message = 'Passwords Do Not Match'
+          return render(request, 'register.html', {'error_message': error_message})
+   return render(request, 'register.html')
+
+# def register(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         email = request.POST['email']
+#         password1 = request.POST['password1']
+#         password2 = request.POST['password2']
+
+#         if password1 == password2:
+#             try:
+#                 user = User.objects.create_user(username, email, password1)
+#                 user.save()
+#                 auth.login(request, user)
+#                 return redirect('chatbot')
+#             except:
+#                 error_message = 'Error creating account'
+#                 return render(request, 'register.html', {'error_message': error_message})
+#         else:
+#             error_message = 'Password dont match'
+#             return render(request, 'register.html', {'error_message': error_message})
+#     return render(request, 'register.html')
+
 def logout(request):
     auth.logout(request)
+    return redirect('login')
